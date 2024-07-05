c4110f4985844f107e06d22a22f43dc254336407
Arame Thiam <60295234+Arame93@users.noreply.github.com>
2024-07-05 14:40:11 +0000

    Update prediction.py

--- Bank_Telemarketing_Campaign_Analysis_and_Prediction/prediction.py
+++ Bank_Telemarketing_Campaign_Analysis_and_Prediction/prediction.py
@@ -2,7 +2,7 @@ import pandas as pd
 import joblib
 import streamlit as st
 
-model = joblib.load('bank_telemarketing_model.joblib')
+model = joblib.load('bank_telemarketing_classifier.pkl')
 
 st.title('Bank Telemarketing Campaign Prediction Model')
 st.write('This app predicts whether a customer will accept or deny the bank term deposit offer.')
